8535ef0a33d9500fce8edb0c8cc10012f185475b
jribacruz <jribacruz@gmail.com>
2017-02-23 23:23:07 -0300

    add pastas e arquivos

--- generator-sk-angular2/generators/app/index.js
+++ generator-sk-angular2/generators/app/index.js
@@ -15,43 +15,60 @@ module.exports = class extends Generator {
         mkdirp(this.options.appname);
         this.destinationRoot(this.destinationPath(this.options.appname));
 
-        //copy package.json
-        this.fs.copyTpl(
-            this.templatePath('package.json'),
-            this.destinationPath('package.json'),
+        mkdirp('src/app/component');
+        mkdirp('src/app/directive');
+        mkdirp('src/app/domain');
+        mkdirp('src/app/service');
+        mkdirp('src/app/view');
+        mkdirp('src/app/view/mngt');
+        mkdirp('src/app/view/sys');
+        mkdirp('src/app/view/user');
+
+        //copy src/app/app.module.ts
+        this.fs.copy(this.templatePath('src/app/app.module.ts'), this.destinationPath('src/app/app.module.ts'));
+
+         //copy src/environments/*
+        this.fs.copy(this.templatePath('src/environments/**'), this.destinationPath('src/environments'));
+
+        this.fs.copy(this.templatePath('src/favicon.ico'), this.destinationPath('src/favicon.ico'));
+
+        this.fs.copyTpl(this.templatePath('src/index.html'),this.destinationPath('src/index.html'),
             {appname: this.options.appname}
         );
 
-        //copy angular-cli.json
-         this.fs.copyTpl(
-            this.templatePath('angular-cli.json'),
-            this.destinationPath('angular-cli.json'),
-            {appname: this.options.appname}
-        );
+        this.fs.copy(this.templatePath('src/main.ts'), this.destinationPath('src/main.ts'));
+
+        this.fs.copy(this.templatePath('src/polyfills.ts'), this.destinationPath('src/polyfills.ts'));
+
+        this.fs.copy(this.templatePath('src/styles.css'), this.destinationPath('src/style.css'));
+
+        this.fs.copy(this.templatePath('src/test.ts'), this.destinationPath('src/test.ts'));
+
+        this.fs.copy(this.templatePath('src/tsconfig.json'), this.destinationPath('src/tsconfig.json'));
+
+        this.fs.copy(this.templatePath('src/typings.d.ts'), this.destinationPath('src/typings.d.ts'));
 
         //copy .gitignore
-        this.fs.copy(
-            this.templatePath('.gitignore'),
-            this.destinationPath('.gitignore')
-        );
+        this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
 
+        //copy angular-cli.json
+         this.fs.copyTpl(this.templatePath('angular-cli.json'),this.destinationPath('angular-cli.json'),
+            {appname: this.options.appname}
+        );
+        
         //copy karma.config.js
-        this.fs.copy(
-            this.templatePath('karma.conf.js'),
-            this.destinationPath('karma.conf.js')
+        this.fs.copy(this.templatePath('karma.conf.js'), this.destinationPath('karma.conf.js'));
+        
+        //copy package.json
+        this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'),
+            {appname: this.options.appname}
         );
 
         //copy protactor.conf.js
-        this.fs.copy(
-            this.templatePath('protractor.conf.js'),
-            this.destinationPath('protractor.conf.js')
-        );
+        this.fs.copy(this.templatePath('protractor.conf.js'), this.destinationPath('protractor.conf.js'));
 
         //copy tslint.json
-        this.fs.copy(
-            this.templatePath('tslint.json'),
-            this.destinationPath('tslint.json')
-        )
+        this.fs.copy(this.templatePath('tslint.json'), this.destinationPath('tslint.json'));
     }
 
 };
---
+++ generator-sk-angular2/generators/app/templates/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { CovalentCoreModule, CovalentLayoutModule } from '@covalent/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule.forRoot(),
+    CovalentCoreModule.forRoot(),
+    CovalentLayoutModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
